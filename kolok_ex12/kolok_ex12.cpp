0b5930988913c9a77e471e7a6c5c57bfd5450481
Vika <Vika@HOME-PC>
2021-11-03 01:24:15 +0300

    Добавьте файлы проекта.

---
+++ kolok_ex12/kolok_ex12.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+int main() {
+	int n;
+	std::cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i];
+	}
+	int k = 0;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 1; j < n; j++) {
+			if (a[i] == a[j]) {
+				k++;
+			}
+		}
+		if (k == 2) {
+			std::cout << a[i];
+			break;
+		}
+		k = 0;
+	}
+	return 0;
+}
